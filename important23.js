ba618b17bb94c0d0064daaad02ed35a91b3d8f7d
Anshumanrohsan <69867365+Anshumanrohsan@users.noreply.github.com>
2020-10-20 14:30:47 +0530

    Add files via upload

---
+++ important23.js
@@ -0,0 +1,13 @@
+(function($){
+    $(function(){
+  
+      $('.sidenav').sidenav({
+          edge: 'right'
+      });
+      $('.parallax').parallax(
+        
+      );
+  
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+  
